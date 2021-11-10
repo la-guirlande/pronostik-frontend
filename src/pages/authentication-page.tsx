6354be15d5fd8fee7b7382b03073172cccad7691
Miki Namsrai <miki.namsrai@gmail.com>
2021-11-10 22:23:50 +0100

    add login logic

---
+++ src/pages/authentication-page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AuthenticationContainer } from '../components/authentication/authentication-container';
+
+export const AuthenticationPage: React.FC = () => {
+    return (
+        <AuthenticationContainer />
+    )
+}
