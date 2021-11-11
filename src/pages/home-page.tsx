3eab4b849da71d2758d38348fa9fe124e08bfb14
Miki Namsrai <miki.namsrai@gmail.com>
2021-11-11 08:41:31 +0100

    update routes & add boolean

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -4,9 +4,8 @@ import { GamePage } from './game-page';
 
 export const HomePage: React.FC = () => {
   return (
-    <div className="md:flex flex-col md:flex-row md:min-h-screen w-screen">
-      <Sidebar />
-      <GamePage />
+    <div className="">
+      home page
     </div>
   )
 }
