7263c138d41b12395e6d8c532e99d96a7fad8921
Lelberto <jsurieux@gmail.com>
2021-11-19 12:15:17 +0100

    Remove unused imports

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Sidebar } from '../components/sidebar/sidebar';
-import { GamePage } from './game-page';
 
 export const HomePage: React.FC = () => {
   return (
